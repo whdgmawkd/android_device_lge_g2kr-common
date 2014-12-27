3ecd426c39f0a63c2bf115da2e4fff9da80e93f4
parkjongheum <parkjongheum@debian>
2014-12-27 16:03:57 +0900

    disable build wifi hal

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -56,6 +56,7 @@ WPA_SUPPLICANT_VERSION      := VER_0_8_X
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
 BOARD_HOSTAPD_DRIVER        := NL80211
 BOARD_HOSTAPD_PRIVATE_LIB   := lib_driver_cmd_bcmdhd
+BOARD_NO_WIFI_HAL           := true
 BOARD_WLAN_DEVICE           := bcmdhd
 WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/bcmdhd/parameters/firmware_path"
 WIFI_DRIVER_FW_PATH_STA     := "/system/etc/firmware/fw_bcmdhd.bin"

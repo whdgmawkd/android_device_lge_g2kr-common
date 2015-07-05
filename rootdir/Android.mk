45ee0e96c948df62aa47a9c65fe4c9dadd333809
parkjongheum <parkjh0821@gmail.com>
2015-07-05 15:36:19 +0900

    Add Init,fstab for F320
    Change-Id: I46c7bcc340a5080632df54853a17ce937519d03f

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -43,3 +43,11 @@ LOCAL_MODULE_CLASS      := ETC
 LOCAL_SRC_FILES         := etc/ueventd.g2.rc
 LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := init.g2.sdcard.rc
+LOCAL_MODULE_TAGS       := optional eng
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := etc/init.g2.sdcard.rc
+LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
